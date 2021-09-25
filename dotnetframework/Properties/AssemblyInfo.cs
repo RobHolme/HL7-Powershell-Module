cc8eee210caeeb275890c17500d7a7ec0efd326e
Rob Holme <rob@holme.com.au>
2021-09-25 13:37:43 +0800

    incremented file version to v1.7.5

--- dotnetframework/Properties/AssemblyInfo.cs
+++ dotnetframework/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robert Holme")]
 [assembly: AssemblyProduct("HL7Tools")]
-[assembly: AssemblyCopyright("Copyright © Robert Holme 2020")]
+[assembly: AssemblyCopyright("Copyright © Robert Holme 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.4.*")]
+[assembly: AssemblyVersion("1.7.5.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
