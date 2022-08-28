53f0d8abf15457df3835ef9eeb21c7aa26f88bf9
Rob Holme <rob@holme.com.au>
2022-08-28 15:07:57 +0800

    Tageting net framework 4.8 instead of 4.5.2

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+del .\module\hl7tools\lib\
+dotnet publish --configuration release --framework netstandard2.0 --output .\module\hl7tools\lib\netstandard2.0\
+dotnet publish --configuration release --framework net48 --output .\module\hl7tools\lib\net48\
