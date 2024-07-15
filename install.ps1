79954c3e229962b4ac1d5403fc1ce4f1a8810151
Rob Holme <rob@holme.com.au>
2024-07-15 18:53:14 +0800

    Fixed issue with -Scope parameter being overwritten

--- install.ps1
+++ install.ps1
@@ -166,7 +166,7 @@ cd module\hl7tools
 
 # Prompt user to select modile if "PromptForModulePath" provided as the -Scope parameter, otherwise use AllUsers or CurrentUser path based on the parameter 
 # Note: -Scope defaults to CurrentUser if no paramter value provided. 
-if($scope = "PromptForModulePath") {
+if($scope -eq "PromptForModulePath") {
     $moduleRootPath = Select-PSModulePath
 }
 else {
